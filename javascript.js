e09400cec44b00e37738c22905b1b572840e1636
lilchix <104845593+lilchix@users.noreply.github.com>
2023-06-19 01:06:11 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,40 +1,37 @@
-function menuOnClick() {
-  // Aggiunge o rimuove la classe "change" per gli elementi corrispondenti
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-}
 
 function aggiungiAlCarrello() {
-  // Incrementa il numero nel carrello
   var carrello = document.getElementById("carrello");
-  var numero = parseInt(carrello.textContent);
+  var numero = parseInt(carrello.innerHTML);
   numero++;
-  carrello.textContent = numero;
+  carrello.innerHTML = numero;
 }
-
 function rimuoviDalCarrello() {
-  // Decrementa il numero nel carrello
   var carrello = document.getElementById("carrello");
-  var numero = parseInt(carrello.textContent);
+  var numero = parseInt(carrello.innerHTML);
   numero--;
-  carrello.textContent = numero;
+  carrello.innerHTML = numero;
 }
-
 function svuotaCarrello() {
-  // Resetta il numero nel carrello a zero
   var carrello = document.getElementById("carrello");
-  carrello.textContent = 0;
+  carrello.innerHTML = 0;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
 
 
-
+/*swiper*/
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+},)
