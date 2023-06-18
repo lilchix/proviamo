6437891c1693e57c66a7cdc89b12e3f595b85e2b
lilchix <104845593+lilchix@users.noreply.github.com>
2023-06-18 18:52:33 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -23,4 +23,19 @@ function svuotaCarrello() {
   carrello.textContent = 0;
 }
 
-
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
