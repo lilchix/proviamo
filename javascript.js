d3cbb58ff92a050cce3628e6b14de78f67d9e034
lilchix <104845593+lilchix@users.noreply.github.com>
2023-06-18 18:36:12 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
 function menuOnClick() {
-  var menu = document.getElementById("nav");
-  var menuBar = document.getElementById("menu-bar");
-  
-  menu.classList.toggle("nav-active");
-  menuBar.classList.toggle("change");
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+function aggiungiAlCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.textContent);
+  numero++;
+  carrello.textContent = numero;
+}
+
+function rimuoviDalCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.textContent);
+  numero--;
+  carrello.textContent = numero;
+}
+
+function svuotaCarrello() {
+  var carrello = document.getElementById("carrello");
+  carrello.textContent = 0;
 }
